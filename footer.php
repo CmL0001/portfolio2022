0c684bd5252553760dcfa2988b57e2b7d4f37aba
Corey Lyons <avs302003@yahoo.com>
2022-07-06 14:34:28 -0600

    changed h3 tag in footer to p tag

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
         </div>
 
         <div class="col-sm-12 col-md-6 form-area text-center">
-          <h3 class="contact-title text-center">Contact Me</h3>
+          <p class="contact-title text-center">Contact Me</p>
           <i class="fas fa-envelope envelope"></i>
           <?php if(is_active_sidebar('contact_shortcode') ) : ?>
             <?php dynamic_sidebar('contact_shortcode'); ?>  
